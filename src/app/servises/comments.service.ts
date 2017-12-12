688fab4fde85abb7d9b82d2a25b965138e3128e9
KoltsovJeka <koltsov1998@mail.ua>
2017-12-12 11:47:44 +0200

    added server for comments

--- src/app/servises/comments.service.ts
+++ src/app/servises/comments.service.ts
@@ -1,20 +1,33 @@
 import { Http,RequestOptions,Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+
+
 @Injectable()
 export class CommentsService {
   private url = 'http://localhost:8080/comments/';
 
+
+
   constructor(private http: Http) { }
+
+
+
 
   get(id: number){
     return this.http.get(this.url + id);
   }
 
+
+
+
   post(post){
     let headers = new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
     return this.http.post(this.url + '/add', post, options);
-  }
+ 
+ }
+
+
 
 }
