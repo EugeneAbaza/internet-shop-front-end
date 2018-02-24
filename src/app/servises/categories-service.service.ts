7b120c97af96060eea9d1cdd15670e2b954cd0ee
Vladimir Kovalchuk <vokovalchuk1995@gmail.com>
2018-02-24 14:22:01 +0200

    category el conflict res 2

--- src/app/servises/categories-service.service.ts
+++ src/app/servises/categories-service.service.ts
@@ -3,11 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CategoriesServiceService {
-<<<<<<< HEAD
   private url = 'https://safe-springs-87091.herokuapp.com';
-=======
-  private url = 'http://safe-springs-87091.herokuapp.com';
->>>>>>> dev2
 
   constructor(private http: HttpClient) { }
 
