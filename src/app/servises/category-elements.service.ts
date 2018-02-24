ae56b13d7bcada6661fc7f102c1cfae009a03aa4
Vladimir Kovalchuk <vokovalchuk1995@gmail.com>
2018-02-24 14:18:00 +0200

    category el conflict res

--- src/app/servises/category-elements.service.ts
+++ src/app/servises/category-elements.service.ts
@@ -5,11 +5,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CategoryElementsService {
 
-<<<<<<< HEAD
   private url = 'https://safe-springs-87091.herokuapp.com/goods/category';
-=======
-  private url = 'http://safe-springs-87091.herokuapp.com/goods/category';
->>>>>>> dev2
   constructor(private http: HttpClient) { }
 
   getElements(id: number, page: number, filters: Filters){
